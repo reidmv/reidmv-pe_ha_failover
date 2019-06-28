d873da7c4d1331b6b60701af6bc8457ca235edae
Reid Vandewiele <reid@puppetlabs.com>
2019-06-28 06:18:52 -0500

    fixup

--- tasks/generate_certificate.rb
+++ tasks/generate_certificate.rb
@@ -1,6 +1,7 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 
 require_relative "../../ruby_task_helper/files/task_helper.rb"
+require 'open3'
 
 class GenerateCertificate < TaskHelper
   def task(certname: nil, **kwargs) 
