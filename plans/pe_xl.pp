a5d69cd28d2fe12d9af31421009fdb499c9b8b45
Reid Vandewiele <reid@puppetlabs.com>
2019-07-02 22:15:28 -0700

    Change pause to service stop, stop on all
    Trying to work around what seems to be PE-26615.

--- plans/pe_xl.pp
+++ plans/pe_xl.pp
@@ -3,20 +3,16 @@ plan pe_ha_failover::pe_xl(
 ) {
 
   $master = puppetdb_query(@("PQL")).map |$node| { $node['certname'] }
-    resources[certname] { 
+    resources[certname] {
       type = "Class" and
       title = "Puppet_enterprise::Profile::Certificate_authority" }
     | PQL
 
   $replica = puppetdb_query(@("PQL")).map |$node| { $node['certname'] }
-    resources[certname] { 
+    resources[certname] {
       type = "Class" and
       title = "Puppet_enterprise::Profile::Primary_master_replica" }
     | PQL
-
-  if ($master.size != 1) or ($replica.size != 1) {
-    fail_plan("Unable to lookup inputs. Found: ${master} for master; ${replica} for replica.")
-  }
 
   $pdb_databases = puppetdb_query(@("PQL")).map |$node| { $node['certname'] }
     resources[certname] {
@@ -26,11 +22,11 @@ plan pe_ha_failover::pe_xl(
       !(certname = "${replica[0]}") }
     | PQL
 
-  # During the promotion process, the old master will be deactivated in
-  # PuppetDB. To ensure that Puppet doesn't remove the RBAC rules allowing the
-  # old master's certname to connect, we temporarily pause Puppet agent runs on
-  # the PuppetDB PostgreSQL nodes.
-  run_task('pe_ha_failover::pause_puppet_agent', $pdb_databases,
+  # During the promotion process, several Puppet agent runs will be
+  # orchestrated.  To avoid a race condition (and bug) between the agent and
+  # orchestrated runs, we will pause self-activated agent runs before beginning
+  # the failover operation.
+  run_task('pe_ha_failover::pause_puppet_agent', $master + $replica + $pdb_databases,
     duration => '30m',
   )
 
--- tasks/pause_puppet_agent.rb
+++ tasks/pause_puppet_agent.rb
@@ -5,23 +5,21 @@ require 'open3'
 
 class PausePuppetAgent < TaskHelper
   def task(duration:, **kwargs) 
-    output, status = Open3.capture2e('/opt/puppetlabs/bin/puppet', 'agent','--disable', "Puppet agent runs paused for #{duration} by Bolt task")
 
-    raise TaskHelper::Error.new('Failed to pause Puppet agent',
-                                'pe_ha_failover/task-error',
-                                output) unless status.success?
-
-    lockfile = Open3.capture2('/opt/puppetlabs/bin/puppet', 'config', 'print', 'agent_disabled_lockfile', '--section', 'agent').first.chomp
-    modified = Open3.capture2('stat', '-c%Y', lockfile).first.chomp
-
-    output, status = Open3.capture2('systemd-run', "--on-active=#{duration}", 'bash', '-c', %Q{[ "$(stat -c%Y #{lockfile})" = "#{modified}" ] && rm "#{lockfile}"})
-
+    # Schedule the service to start back up later
+    output, status = Open3.capture2('systemd-run', "--on-active=#{duration}", '--unit', 'puppet.service')
     raise TaskHelper::Error.new('Failed to schedule re-enable action for Puppet agent',
                                 'pe_ha_failover/task-error',
                                 output) unless status.success?
 
+    # Stop the service for now
+    output, status = Open3.capture2e('systemctl', 'stop','puppet.service')
+    raise TaskHelper::Error.new('Failed to pause Puppet agent',
+                                'pe_ha_failover/task-error',
+                                output) unless status.success?
+
     {}
-  end 
+  end
 end
 
 if __FILE__ == $0
